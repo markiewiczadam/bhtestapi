19ccce642afbc05327d118c8732b981deb78b35e
adammarkiewicz <a_markiewicz@outlook.com>
2022-07-29 16:02:14 +0200

    1st with db

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from _config import *
-from fastapi import FastAPI, Request
 #import uvicorn
+from fastapi import FastAPI, Request
+from apiFunctions import *
+
 
 app = FastAPI(title=fastapi_title, version=fastapi_version, contact=fastapi_contact)
 
 
 @app.get("/")
-async def root():
+def root():
     return {"message": "Hello Azure"}
 
 @app.get("/get")
-async def get_name(name:str):
+def get_name(name:str):
     return {"name": name}
+
+@app.get("/getClientCorporation/{client_corporation_id}")
+def getClientCorporation(client_corporation_id:int):
+    resp = ClientCorporation(client_corporation_id)
+    return resp
