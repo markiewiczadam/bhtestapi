db3a436b20a92367485020d072ec58ee7cccc6f1
adammarkiewicz <a_markiewicz@outlook.com>
2022-07-29 13:59:00 +0200

    2nd commit

---
+++ _config.py
@@ -0,0 +1,4 @@
+##### FastAPI Swagger description #####
+fastapi_title = 'Bullhorn Reporting DB API'
+fastapi_version = '0.0.1'
+fastapi_contact = {'name': 'Adam Markiewicz', 'email': 'adam.markiewicz@scalosoft.com'}
