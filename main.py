b4a1698754ec93bf300c557ff617cafbf8c7ca08
adammarkiewicz <a_markiewicz@outlook.com>
2022-08-01 16:52:57 +0200

    getVacancy added

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from _config import *
-#import uvicorn
 from fastapi import FastAPI, Request
 from apiFunctions import *
 
@@ -10,13 +9,13 @@ app = FastAPI(title=fastapi_title, version=fastapi_version, contact=fastapi_cont
 ###########
 
 @app.get("/getClientCorporationDetails/{client_corporation_id}")
-def get_Client_Corporation_Details(client_corporation_id:int):
+def get_Client_Corporation_Details(client_corporation_id: int):
     resp = clientCorporationDetails(client_corporation_id)
     return resp
 
 
 @app.get("/getVacancyDetails/{vacancy_id}")
-def get_Vacancy_Details(vacancy_id:int):
+def get_Vacancy_Details(vacancy_id: int):
     resp = vacancyDetails(vacancy_id)
     return resp
 
