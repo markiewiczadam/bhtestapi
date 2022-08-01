a89804b931cbc2eb80770eeff01483ce7aaf7b1c
adammarkiewicz <a_markiewicz@outlook.com>
2022-08-01 13:07:25 +0200

    getVacancy added

--- _config.py
+++ _config.py
@@ -1,6 +1,6 @@
 ##### FastAPI Swagger description #####
 fastapi_title = 'Bullhorn Reporting DB API'
-fastapi_version = '0.0.1'
+fastapi_version = '0.1.0 beta'
 fastapi_contact = {'name': 'Adam Markiewicz', 'email': 'adam.markiewicz@scalosoft.com'}
 
 
